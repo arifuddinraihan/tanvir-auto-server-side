2e61b126e6dbd05b1cdc4a486e8dd0921414e672
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-11-05 15:19:18 +0600

    added delete api for order path with dynamic id

--- index.js
+++ index.js
@@ -51,6 +51,13 @@ async function run(){
             res.send(result)
         })
 
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id : ObjectId(id)};
+            const result = await orderCollection.deleteOne(query)
+            res.send(result)
+        })
+
     }
     finally{
 
