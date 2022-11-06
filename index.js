1531caa4316581fc72a8c6cbff9147ae35618a1d
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-11-06 17:06:13 +0600

    added verifyJWT func and added its functionalityinside get . orders API

--- index.js
+++ index.js
@@ -15,18 +15,43 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    // received information from client side
+    console.log("Inside orders API with authoraization from Client", req.headers.authoraization)
+    const authHeader = req.headers.authoraization
+    if (!authHeader) {
+        res.status(401).send({ message: "unauthorized access" })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            res.status(401).send({ message: "unauthorized access" })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
 async function run() {
     try {
         const serviceCollection = client.db('tanvirAutosDB').collection('services')
         const orderCollection = client.db('tanvirAutosDB').collection('orders')
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" })
+            res.send({ token })
+        })
+
         app.get('/services', async (req, res) => {
             const page = parseInt(req.query.page);
             const size = parseInt(req.query.size);
             const query = {};
             const cursor = serviceCollection.find(query);
-            const services = await cursor.skip(page*size).limit(size).toArray();
+            const services = await cursor.skip(page * size).limit(size).toArray();
             const count = await serviceCollection.estimatedDocumentCount()
-            res.send({count , services})
+            res.send({ count, services })
         })
 
         app.get('/services/:id', async (req, res) => {
@@ -36,7 +61,15 @@ async function run() {
             res.send(service)
         })
 
-        app.get('/orders', async (req, res) => {
+        app.get('/orders', verifyJWT, async (req, res) => {
+            // received decoded from verifyJWT function
+            const decoded = req.decoded;
+            console.log("Inside orders API with decoded from req headers", decoded)
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: "unauthorized access" })
+            }
+            // steps above this line need to be wirte after creating verifyJWT function
+
             let query = {}
             if (req.query.email) {
                 // make an email filter via-query 
@@ -62,7 +95,7 @@ async function run() {
             // Updated value to set under this variable with value
             const updateDoc = {
                 $set: {
-                    status : status
+                    status: status
                 },
             };
             // Need to send quey along with updated doc value to receive via client patch method
